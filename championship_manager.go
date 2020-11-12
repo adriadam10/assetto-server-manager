5a1f2ce3070e10cc7bfcbce89273a37d68e0db3b
Henry Spencer <henry@icj.me>
2020-11-12 12:00:43 +0000

    added changelog, make sure the running practice is from the championship the driver signed up for

--- championship_manager.go
+++ championship_manager.go
@@ -1547,7 +1547,7 @@ func (cm *ChampionshipManager) AddEntrantFromSessionData(championship *Champions
 			logrus.Errorf("Couldn't add entrant (GUID: %s, Name: %s) to autofill list", newEntrant.GUID, newEntrant.Name)
 		}
 
-		if cm.process.Event().IsPractice() {
+		if cm.process.Event().IsPractice() && cm.process.Event().EventName() == championship.Name+" - Looping Practice" {
 			err = cm.raceControl.server.AddDriver(newEntrant.Name, newEntrant.Team, newEntrant.GUID, entrant.Model)
 
 			if err != nil {
